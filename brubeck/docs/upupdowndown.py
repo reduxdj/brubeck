04828b2f75d79197886dce9f9db301b91d69e338
Patrick Lemiuex <patricklemiuex@gmail.com>
2013-09-23 08:41:54 -0400

    removed duped dir

--- brubeck/docs/upupdowndown.py
+++
@@ -1,5 +0,0 @@
-### Could live in settings
-src_dir = './'
-html_dir = '../../brubeck.io/'
-header_file = '%s%s' % (html_dir + 'media/', 'header.html')
-footer_file = '%s%s' % (html_dir + 'media/', 'footer.html')
